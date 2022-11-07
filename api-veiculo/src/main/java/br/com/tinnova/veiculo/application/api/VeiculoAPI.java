1c100d87c355c6f7f1034e46b02ad1061befe5bc
felipeHenriquePedroso <felipehenrique.pds@gmail.com>
2022-11-07 09:12:04 -0300

    Adiciona Cor aos atributos de VeiculoListResponse

--- api-veiculo/src/main/java/br/com/tinnova/veiculo/application/api/VeiculoAPI.java
+++ api-veiculo/src/main/java/br/com/tinnova/veiculo/application/api/VeiculoAPI.java
@@ -10,31 +10,38 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/v1/veiculo")
 public interface VeiculoAPI {
+    @CrossOrigin
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     VeiculoResponse postVeiculo(@RequestBody @Valid VeiculoRequest veiculoRequest);
 
+    @CrossOrigin
     @GetMapping(value = "/veiculos")
     @ResponseStatus(code = HttpStatus.OK)
     List<VeiculoListResponse> getTodosVeiculos();
 
+    @CrossOrigin
     @GetMapping(value = "/{idVeiculo}")
     @ResponseStatus(code = HttpStatus.OK)
     VeiculoDetalhadoResponse getVeiculoAtravesId(@PathVariable UUID idVeiculo);
 
+    @CrossOrigin
     @PutMapping(value = "/{idVeiculo}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void  putAlteraVeiculo(@PathVariable UUID idVeiculo,
                            @Valid @RequestBody VeiculoAlteracaoRequest veiculoAlteracaoRequest);
-
+    @CrossOrigin
     @PatchMapping(value = "/{idVeiculo}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void  patchAlteraVeiculo(@PathVariable UUID idVeiculo,
                                      @Valid @RequestBody VeiculoAlteraStatusRequest veiculoAlteraStatusRequest);
+
+    @CrossOrigin
     @DeleteMapping(value = "/{idVeiculo}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void  deletaVeiculoAtravesId(@PathVariable UUID idVeiculo);
 
+    @CrossOrigin
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     List<VeiculoListByParametrosResponse> listaVeiculosPorParametros(@RequestParam(value = "marca", required = false) String marca,
